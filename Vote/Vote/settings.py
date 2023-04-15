9b7219afc536e4b927014fc57d1d8ee214f6de73
ragha1v <106020992+ragha1v@users.noreply.github.com>
2023-04-15 20:00:25 +0530

    Update settings.py

--- Vote/Vote/settings.py
+++ Vote/Vote/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7n4#hn95hpqt#%+nam2@rz+s^*k3+t(o$$x@cxtb95p4hb4439
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
